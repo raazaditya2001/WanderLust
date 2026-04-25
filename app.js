922e654957c6afbc7acd3cf4c87bda8071178cda
Banti Kumar <raazaditya2001@gmail.com>
2026-04-25 11:31:40 +0530

    app.js updated

--- app.js
+++ app.js
@@ -5,6 +5,29 @@ const Listing =require("./models/listing.js");
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
+const wrapAsync=require("./utils/wrapAsync.js");
+const expressError=require("./utils/expressError.js");
+const {listingSchema}=require('./schemaValidation/schema.js');
+const Review =require("./models/review.js");
+const {reviewSchema} = require("./schemaValidation/reviewSchema.js");
+const session =require("express-session");
+const flash=require('connect-flash');
+// passport
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js')
+
+// dot env
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config();
+}
+
+
+//routes
+const listing=require('./routes/listing.js');
+const reviews=require('./routes/review.js');
+const user = require('./routes/user.js');
+
 
 
 app.set("view engine","ejs");
@@ -13,6 +36,7 @@ app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.engine('ejs',ejsMate);
+
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -26,75 +50,54 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
-//root route
-app.get("/",(req,res)=>{
-    res.send("hi, I am root");
+const sessionOptions={
+    secret:process.env.SECRET,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge:7 * 24 * 60 * 60 * 1000,
+        httpOnly:true
+    }
+};
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+//passport 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//flash
+app.use((req,res,next)=>{
+    res.locals.successMsg=req.flash("success");
+    res.locals.errorMsg=req.flash("error");
+    res.locals.currUser=req.user;
+    next();
 });
 
-//index route
-app.get("/listings",async (req,res)=>{
-    const allListings=await Listing.find();
-    res.render("listings/index.ejs",{allListings});
-    
+
+//argument handler
+app.use("/listings",listing);
+app.use("/listings/:id/reviews",reviews);
+app.use("/user",user);
+
+
+app.use((req,res,next)=>{
+     next(new expressError(404,"Not Found"));
 });
 
-//new form
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
-   
-
+app.use((err,req,res,next)=>{
+    let {status=500,message="something went wrong"}=err;
+    res.status(status).render("listings/error.ejs",{message,status});
 });
 
-//show route
-app.get("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
-    const listing =await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
-});
 
-//create route
-app.post("/listings",async(req,res)=>{
-    const newListing=new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-    
-})
-
-//edit route
-app.get("/listings/:id/edit",async(req,res)=>{
-    let {id}=req.params;
-    let listing=await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-})
-
-//update route
-app.put("/listings/:id", async(req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect("/listings");
-
-});
-
-//destroy route
-app.delete("/listings/:id",async (req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-})
-
-
-
-// app.get("/testListing",async (req,res)=>{
-//     let sampleListing=new Listing({
-//         title:"My New Villa",
-//         description:"By the beach",
-//         price:1200,
-//         location:"Calangute,Goa",
-//     });
-//     await sampleListing.save();
-//     console.log("sample save successfull");
-//     res.send("successful");
-// })
 
 app.listen(8080,()=>{
     console.log("server is listening");
